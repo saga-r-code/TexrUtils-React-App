fade9166dc172d678d4580bc102d59695aa42a97
saga-r-code <shah.sagar2006@gmail.com>
2024-06-12 20:15:54 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import	image from '../Assets/Color Mode.png'
 
 const Navbar = ({
   title = "Title Here",
@@ -11,16 +12,16 @@ const Navbar = ({
   return (
     <div>
       <nav
-        className={`bg-blue-950 text-white p-4`}
+        className={`bg-blue-950 text-white p-2 md:p-4`}
       >
         <div className="container mx-auto flex justify-between items-center">
-          <ul className="flex gap-10 justify-center items-center">
+          <ul className="flex gap-4 md:gap-10 justify-center items-center">
             <li>
-              <Link className=" text-3xl font-bold cursor-pointer" to="/">
+              <Link className="text-xl md:text-3xl font-bold cursor-pointer" to="/">
                 {title}
               </Link>
             </li>
-            <div className="flex gap-5 ">
+            <div className="flex justify-center items-center gap-3 md:gap-5 ">
               <li>
                 <Link
                   className="hover:underline active:underline visited:underline focus:underline"
@@ -47,6 +48,10 @@ const Navbar = ({
             <div className="bg-yellow-500  w-8 h-8 rounded-full shadow-lg hover:shadow-gray-500" onClick={()=>{togglemode('yellow')}}></div>
             <div className="bg-black w-8 h-8 rounded-full shadow-lg hover:shadow-gray-500" onClick={()=>{togglemode('black')}}></div>
             <div className="bg-white w-8 h-8 rounded-full shadow-lg hover:shadow-gray-500" onClick={()=>{togglemode('white')}}></div>
+          </div>
+
+          <div className="img visible md:hidden">
+            <img src={image} alt="colorMode" className="w-12 h-10 mix-blend-screen" />
           </div>
         </div>
       </nav>
